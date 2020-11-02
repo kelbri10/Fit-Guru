44a26777f9fb74e10d253755d8493df189b87a1a
kelbri10 <kelbri10@gmail.com>
2020-11-02 03:27:07 -0500

    updated api-routes.js with get/post/put requests to api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,14 +12,45 @@ module.exports = app => {
     }); 
 
     app.put('/api/workouts/:id', (req, res) => {
+        const id = req.params.id; 
+        const body = req.body; 
 
+        db.Workout.findOneAndUpdate(
+            {
+                _id: id
+            }, 
+            {
+                $push: { exercises: body }
+            }, 
+            {
+                new: true
+            })
+            .then(dbWorkout => { 
+                res.json(dbWorkout); 
+            })
+            .catch(err => { 
+                res.json(err); 
+            });
     }); 
 
     app.get('/api/workouts/range', (req, res) => {
-
+        db.Workout.find({})
+        .then(dbWorkout => { 
+            res.json(dbWorkout); 
+        })
+        .catch(err => {
+            res.json(err); 
+        });
     }); 
 
     app.post('/api/workouts', (req, res) => {
-
-    })
+        const body = req.body; 
+        db.Workout.create(body)
+        .then(dbWorkout => { 
+            res.json(dbWorkout); 
+        })
+        .catch(err => { 
+            res.json(err); 
+        }); 
+    });
 }
